22dd3e2270f19690ce2adea2272b01e8da6874bc
Peter Sojka <ishi@kakesu.fun>
2023-10-24 18:11:01 +0200

    content - references

--- routes/index.tsx
+++ routes/index.tsx
@@ -23,6 +23,74 @@ export default function Home() {
           týmům přicházet na zcela nečekané způsoby, jak dosahovat svých cílů
           nejen v osobní, ale i profesní sféře.
         </p>
+        <h2>Reference</h2>
+
+        <blockquote>
+          <p>
+            Marty empatický a přesto zcela profesionální přístup mě rozmluvil
+            víc, než bych si dokázala připustit. Dokonalé vedení a jasná
+            struktura byly tou správnou cestou podstatě problém. Pomáhá utřídit
+            si myšlenky a převádí na cesty, které by člověk jen tak nenašel.
+          </p>
+          <footer>- Bc. Jana Homolová,</footer>
+          <cite>hypoteční specialistka</cite>
+        </blockquote>
+
+        <blockquote>
+          <p>
+            Schůzka u Marti mi dala super pohled z druhé strany problému.
+            Dokázala jsem se vžít do role druhého člověka a více vnímat jeho
+            potřeby a že všechno není jen jednostranné. Cítila jsem se dobře, s
+            Marti se známe delší dobu a neměla jsem problém otevřeně popsat, co
+            potřebuji vyřešit. Jsem ráda že mě Marta dokázala držet u tématu,
+            protože to co potřebuje řešit a zpracovávat, mě často odvádí od
+            důležitých bodů a Marti byla v tomhle naprosto profesionální.
+          </p>
+          <footer>Bc. Monika Harantová,</footer>
+          <cite>Sales director</cite>
+        </blockquote>
+
+        <blockquote>
+          <p>
+            Marta mi pomohla jako osobní kouč. Ukázala mi, že všechny odpovědi
+            už mám v sobě. Navedla mě na jiný úhel pohledu na určitý problém.
+          </p>
+          <footer>- Zuzana Dolejšová,</footer>
+          <cite>recepční</cite>
+        </blockquote>
+
+        <blockquote>
+          <p>
+            U Marti jsem byla na koučovací hodině. Ze začátku se mi nezdálo moc
+            pravděpodobné, že by mi to jakkoliv mohl pomoci. S Marti se známe,
+            je moc milá fajn ženská, tak jsem ani nebyla nervózní, ale jak mi
+            může pomoci? Na konci hodiny jsem brečela jak želva, protože účelem
+            její služby není mi říct, jak to mám udělat, ale přijít si na to
+            sama, což se pod jejím vedením podařilo! Bylo to neuvěřitelné,
+            děkuji ti za to. Vřele tě všem doporučuji.
+          </p>
+          <footer>- Petra Jozífová,</footer>
+          <cite>make-up artist</cite>
+        </blockquote>
+
+        <blockquote>
+          <p>
+            Známe to všichni, chceme se v něčem zdokonalit, ale práce a rodina
+            nám zabírají tolik času, že představa chodit někam na kroužek je
+            nemyslitelná, natož pak dělat doma ještě nějaké domácí úkoly. Když
+            ale já se sama nedonutím udělat si nějaký plán, natož ho ještě
+            striktně dodržovat. Jen z té představy, že zase něco musím, je mi
+            špatně. Pokud to máte stejně jako já, jděte na koučink k Martě.
+            Možná to zní trochu jako nábor do sekty, ale nebojte se, společně s
+            ní najdete svoji vnitřní motivací, pomůže vám najít cestu a určitý
+            směr k vysněnému cíli, ať už je jakýkoliv. Jako bonus vás nabije
+            pozitivní energií, která z ní pokaždé vyzařuje. Nemůžete nic
+            ztratit, pouze získat. Za mě vřele doporučuji.
+          </p>
+          <footer>- Ing. Monika Ulčová,</footer>
+          <cite>pedagog</cite>
+        </blockquote>
+        
         <Counter count={count} />
       </div>
     </div>
