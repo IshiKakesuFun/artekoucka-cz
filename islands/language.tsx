e3aa32e87547bf75a01d3233e92c4be60e81c76e
Peter Sojka <ishi@kakesu.fun>
2023-11-01 10:38:40 +0100

    language change button

--- islands/language.tsx
+++ islands/language.tsx
@@ -1,17 +1,17 @@
-import { State } from "../routes/_middleware.ts";
+import { State } from "/routes/_middleware.ts";
 
 export default function Language({ lang }: { lang: State["lang"] }) {
   return (
     <button
       class="hover:(text-gray-900 dark:text-gray-100) font-bold focus:outline-none"
       onClick={() => {
-        lang === "en"
-          ? (document.cookie = "lang=cs")
-          : (document.cookie = "lang=en");
+        lang === "cs"
+          ? (document.cookie = "lang=en")
+          : (document.cookie = "lang=cs");
         location.reload();
       }}
     >
-      {lang === "en" ? "CS" : "EN"}
+      {lang === "cs" ? "EN" : "CS"}
     </button>
   );
 }
