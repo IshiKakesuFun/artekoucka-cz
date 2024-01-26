8ace458ee2b714c0d89641c1c71f3f5b59132b22
Peter Sojka <ishi@kakesu.fun>
2024-01-26 21:32:06 +0100

    testimonials in flex columns

---
+++ components/testimonials/testimonial-column.tsx
@@ -0,0 +1,11 @@
+import { TestimonialProps } from "@@/testimonials/testimonials.tsx";
+import Testimonial from "@@/testimonials/testimonial.tsx";
+
+export default function TestimonialColumn(props: TestimonialProps) {
+  return (
+    <div class="testimonial-column">
+      {props.testimonials?.filter((f) => f.column == props.column)
+        .map((item) => <Testimonial {...item} />)}
+    </div>
+  );
+}
--- components/testimonials/testimonials.tsx
+++ components/testimonials/testimonials.tsx
@@ -1,14 +1,16 @@
-import Testimonial from "@@/testimonials/testimonial.tsx";
+import TestimonialColumn from "@@/testimonials/testimonial-column.tsx";
 
 export type TestimonialType = {
   text: string;
   name: string;
   role: string;
   avatar: string;
+  column?: number;
 };
 
 export type TestimonialProps = {
   testimonials: TestimonialType[];
+  column: number;
 };
 
 export default function Testimonials(props: TestimonialProps) {
@@ -17,7 +19,9 @@ export default function Testimonials(props: TestimonialProps) {
       <div class="container">
         <h2 class="heading-2">Reference</h2>
         <div class="testimonials">
-          {props.testimonials?.map((item) => <Testimonial {...item} />)}
+          {Array.from({ length: props.column }).map((_item, index) => (
+            <TestimonialColumn {...props} column={index} />
+          ))}
         </div>
       </div>
     </section>
