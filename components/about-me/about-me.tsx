4323b4a87d5a5ca91b94a9e7c1783cf9c2bb8a71
Peter Sojka <ishi@kakesu.fun>
2024-02-08 18:07:44 +0100

    about me card construct

--- components/about-me/about-me.tsx
+++ components/about-me/about-me.tsx
@@ -1,9 +1,29 @@
-import { PageProps } from "$fresh/server.ts";
-
-export default function AboutMe(props: PageProps) {
+export default function AboutMe() {
   return (
     <section class="section-about-me" id="about-me">
       <div class="container">
+        <div class="about-me">
+          <div
+            class="about-me-img-box"
+            role="img"
+            aria-label="Artekoučka Marta Sojková sedící venku s paletou v klíně"
+          >
+          </div>
+          <div class="about-me-text-box">
+            <h2 class="heading-3">Mgr. Marta Sojková, MBA</h2>
+            <p class="about-me-text">
+              Otevřete svou mysl v porostorách výtvarného ateliéru. Zde
+              neexistují chyby, pouze příležitosti k dalšímu zkoumání a
+              objevování. Vaše paleta je nekonečná, tak jako vaše schopnost
+              tvořit. Vyjděte za hranice obvyklého. Najděte inspiraci v
+              nečekaných zdrojích. Objevte nová řešení pro individální i týmový
+              růst.
+            </p>
+            <a href="/o-mne/" class="btn btn--outline">
+              Více o mně &rarr;
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
